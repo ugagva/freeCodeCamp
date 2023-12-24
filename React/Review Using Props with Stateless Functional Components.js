814d10eb77126556be3f4b29544eb0e712faf876
ugagva <jgorelova@mail.ru>
2023-12-24 20:37:20 +0600

    Review Using Props with Stateless Functional Components

---
+++ React/Review Using Props with Stateless Functional Components.js
@@ -0,0 +1,30 @@
+//The code editor has a CampSite component that renders a Camper component as a child. Define the Camper component and assign it default props of { name: 'CamperBot' }. Inside the Camper component, render any code that you want, but make sure to have one p element that includes only the name value that is passed in as a prop. Finally, define propTypes on the Camper component to require name to be provided as
+// a prop and verify that it is of type string.
+
+
+//
+class CampSite extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Camper name={"fdfds"}/>
+            </div>
+        );
+    }
+};
+// Change code below this line
+
+const Camper = (props) => {
+    return <h1> Please, park your camper
+        <p>{props.name}</p> after labeles !
+    </h1>
+};
+
+Camper.defaultProps = { name: "CamperBot"};
+Camper.propTypes = {
+    name:PropTypes.string.isRequired};
+
+
