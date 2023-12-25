20822c0ded0cbd8684b2fba67401d46463acbe49
ugagva <jgorelova@mail.ru>
2023-12-25 16:52:50 +0600

    Use State to Toggle an Element

---
+++ React/Use State to Toggle an Element.js
@@ -0,0 +1,53 @@
+//Use State to Toggle an Element
+
+//MyComponent has a visibility property which is initialized to false. The render method returns one view if the value of visibility is true, and a different view if it is false.
+//
+// Currently, there is no way of updating the visibility property in the component's state. The value should toggle back and forth between true and false. There is a click handler on the button which triggers a class method called toggleVisibility(). Pass a function to setState to define this method so that the state of visibility toggles to the opposite value when the method is called. If visibility is false, the method sets it to true, and vice versa.
+//
+// Finally, click the button to see the conditional rendering of the component based on its state.
+//
+// Hint: Don't forget to bind the this keyword to the method in the constructor!
+
+
+
+//SOLUTION
+
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibility: false
+        };
+        // Change code below this line
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+        // Change code above this line
+    }
+    // Change code below this line
+    toggleVisibility(){
+        this.setState(state=>{
+                if(state.visibility==true){
+                    return {visibility:false}
+                } else return {visibility:true}
+            }
+
+        )
+    }
+    // Change code above this line
+    render() {
+        if (this.state.visibility) {
+            return (
+                <div>
+                    <button onClick={this.toggleVisibility}>Click Me</button>
+                    <h1>Now you see me!</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <button onClick={this.toggleVisibility}>Click Me</button>
+                </div>
+            );
+        }
+    }
+}
