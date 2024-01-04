d5235fd2bf29996f5d9354d45ea93972c612b650
ugagva <jgorelova@mail.ru>
2024-01-04 17:47:38 +0600

    Array.diff

---
+++ Codewars/Arraydiff.js
@@ -0,0 +1,19 @@
+/
+
+
+
+/function arrayDiff(a, b) {
+//
+//    let myArray =getObject(b);
+//
+//   return a.filter(el=> !myArray[el]);
+// }
+//
+// function getObject(arr){
+//   var obj={}
+//
+//   for (var n of arr){
+//     obj[n]=true;
+//   }
+//   return obj;
+// }
