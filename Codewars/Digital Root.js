b4fd2ce9124b00edacd3b4c4d712d083e6e8dc58
ugagva <jgorelova@mail.ru>
2024-01-06 14:05:56 +0600

    Digital Root

---
+++ Codewars/Digital Root.js
@@ -0,0 +1,10 @@
+//Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+//
+// Examples
+
+
+//function digitalRoot(n) {
+//   return (n-2)%9+1;
+//
+//   }
+//
